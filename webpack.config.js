80a3ccd5ec8bfd70104b8520085ecbe5250139a6
limuran <LIMURAN247@pingan.com.cn>
2018-02-02 15:21:18 +0800

    fix(resume): update

--- webpack.config.js
+++ webpack.config.js
@@ -1,56 +1,40 @@
 const path = require('path');
-const { WebPlugin } = require('web-webpack-plugin');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
-
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  output: {
-    publicPath: '',
-    filename: '[name].js',
-  },
-  resolve: {
-    // 加快搜索速度
-    modules: [path.resolve(__dirname, 'node_modules')],
-    // es tree-shaking
-    mainFields: ['jsnext:main', 'browser', 'main'],
-  },
-  module: {
-    rules: [
-      {
-        test: /\.scss$/,
-        // 提取出css
-        loaders: ExtractTextPlugin.extract({
-          fallback: 'style-loader',
-          use: ['css-loader', 'sass-loader']
-        }),
-        include: path.resolve(__dirname, 'src')
-      },
-      {
-        test: /\.css$/,
-        // 提取出css
-        loaders: ExtractTextPlugin.extract({
-          fallback: 'style-loader',
-          use: ['css-loader'],
-        }),
-      },
-      {
-        test: /\.(gif|png|jpe?g|eot|woff|ttf|svg|pdf)$/,
-        loader: 'base64-inline-loader',
-      },
-    ]
-  },
-  entry: {
-    main: './src/main.js',
-  },
-  plugins: [
-    new WebPlugin({
-      template: './src/index.html',
-      filename: 'index.html',
-    }),
-    new ExtractTextPlugin({
-      filename: '[name].css',
-      allChunks: true,
-    }),
-  ],
-  devtool: 'source-map',
+    entry: './src/main.js',
+    output: {
+        path: path.resolve(__dirname, './dist/index.html'),
+        filename: './dist/bundle.js'
+    },
+    resolve: {
+        // 加快搜索速度
+        modules: [path.resolve(__dirname, 'node_modules')]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({filename: 'index.html', template: 'index.html', inject: 'head'}),
+        new ExtractTextPlugin({filename: '[name].css', allChunks: true})
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.scss$/,
+                // 提取出css
+                loaders: ExtractTextPlugin.extract({
+                    fallback: 'style-loader',
+                    use: ['css-loader', 'sass-loader']
+                }),
+                include: path.resolve(__dirname, 'src')
+            }, {
+                test: /\.css$/,
+                // 提取出css
+                loaders: ExtractTextPlugin.extract({fallback: 'style-loader', use: ['css-loader']})
+            }, {
+                test: /\.(gif|png|jpe?g|eot|woff|ttf|svg|pdf)$/,
+                loader: 'base64-inline-loader'
+            }
+        ]
+    },
+    devtool: 'source-map'
 };
